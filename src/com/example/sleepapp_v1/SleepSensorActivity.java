91e69bc235db6a6a19b1bf2793018f6183c8a8ce
barbara42 <barbara.raven42@gmail.com>
2013-12-06 00:17:31 -0500

    improved sleep sensors

--- src/com/example/sleepapp_v1/SleepSensorActivity.java
+++ src/com/example/sleepapp_v1/SleepSensorActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.util.Log;
 import android.view.Menu;
 
@@ -17,17 +18,31 @@ public class SleepSensorActivity extends Activity {
 	  private Sensor mLight;
 	  private Sensor mAccelerometer; 
 	  private Sensor mTemperature; 
+	  private boolean hasLight = false; 
+	  private boolean hasAccel = false; 
+	  private boolean hasTemp = false; 
 	  
 	  @Override
 	  public final void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_sleep_sensor);
-
+	    
+	    //check which sensors are available on the phone
+	    
+//	    PackageManager packageManager = this.getPackageManager();
+//	    boolean hasLightSensor = packageManager.hasSystemFeature(PackageManager.FEATURE_SENSOR_LIGHT);
+//	    boolean hasAccelerometer = packageManager.hasSystemFeature(PackageManager.FEATURE_SENSOR_ACCELEROMETER);
+//	    boolean hasTempSensor = packageManager.hasSystemFeature(PackageManager.FEATURE_);
+	    
 	    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+	    //check which sensors are available on the phone
 	    mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 	    mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 	    mTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
-	    
+	    if(mLight != null) hasLight = true; 
+	    if(mAccelerometer != null) hasAccel = true; 
+	    if(mTemperature != null) hasTemp = true; 
+
 	    mSensorListener = new SensorEventListener(){
 	    	@Override
 	    	public void onAccuracyChanged(Sensor arg0, int arg1){
@@ -40,10 +55,10 @@ public class SleepSensorActivity extends Activity {
 	    			Log.w("Accel Sensor", "Accel: " + accel);
 	    		}else if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE){
 	    			float temp = event.values[0];
-	    			//Log.w("Temp Sensor", "temp: " + temp);
+	    			Log.w("Temp Sensor", "temp: " + temp);
 	    		}else if (sensor.getType() == Sensor.TYPE_LIGHT){
 	    			float lux = event.values[0];
-	    			//Log.w("Light Sensor", "Light: " + lux);
+	    			Log.w("Light Sensor", "Light: " + lux);
 	    		}
 	    	}
 	    };
@@ -53,10 +68,9 @@ public class SleepSensorActivity extends Activity {
 	  protected void onResume() {
 	    super.onResume();
 	    Log.w("Resume", "Sleep Sensor Activity Resumed");
-	    //mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
-	    mSensorManager.registerListener(mSensorListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-	    mSensorManager.registerListener(mSensorListener, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
-	    mSensorManager.registerListener(mSensorListener, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+	    if (hasAccel) mSensorManager.registerListener(mSensorListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	    if (hasTemp) mSensorManager.registerListener(mSensorListener, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+	    if (hasLight) mSensorManager.registerListener(mSensorListener, mLight, SensorManager.SENSOR_DELAY_NORMAL);
 	  }
 
 	  @Override
